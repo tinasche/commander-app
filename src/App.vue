7a47ac690c7c5bdcb2c4612c072fd43702575324
Tinashe Chitakunye <tinashec93@live.com>
2023-01-22 10:43:19 +0200

    feat: connect with .net backend

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
-
 <template>
   <div class="container" id="main">
     <h2>Commands App</h2>
-
-    <div id="commandList" v-for="command in commandStore.commandsList" :key="command.Id">
-      <CommandItem :commandString="command.CommandString" :commandDesc="command.Description" :commandPlatform="command.Platform" />
+    <div v-if="commandStore.getAllCommands != null">
+      <div v-for="command in commandStore.getAllCommands" :key="command.id"> 
+        <CommandItem :commandString="command['commandString']" :commandDesc="command['description']"  :commandPlatform="command['platform']"/>
+      </div>
     </div>
+    <div id="noContent" v-else>
+      <h4>Nothing to see here</h4>
+    </div>
+
 
   </div>
 </template>
@@ -17,8 +21,20 @@ export default {
   components: { CommandItem },
   setup() {
     const commandStore = useCommandsStore()
+    commandStore.getCommands()
 
     return { commandStore }
-  }
+  },
+
+  
+  
 }
 </script>
+
+<style>
+#noContent{
+  text-align: center;
+  margin: 2rem;
+  font-style: italic;
+}
+</style>
--- src/stores/commandsStore.js
+++ src/stores/commandsStore.js
@@ -1,35 +1,27 @@
 import { defineStore } from "pinia";
+import axios  from "axios";
+
+axios.defaults.baseURL = "https://localhost:7188/api/Commands"
 
 export const useCommandsStore = defineStore("commandsStore", {
   state: () => ({
-    commandsList: [
-      {
-        Id: 1,
-        CommandString: "dotnet watch run",
-        Description:
-          "executes dotnet with 'watch' parameter for file changes and hot reloads",
-        Platform: "dotnet",
-      },
-      {
-        Id: 2,
-        CommandString: "dotnet build",
-        Description: "build the current project/solution",
-        Platform: "dotnet",
-      },
-      {
-        Id: 3,
-        CommandString: "npm init vue@latest",
-        Description: "start a new vue project",
-        Platform: "vuejs",
-      },
-    ],
-    }),
-    actions: {
-
-    },
-    getters: {
-        getAllCommands: function () {
-            return this.commandsList
-        }
+    commandsList: ["something"],
+  }),
+  actions: {
+    // FIXME: resolve delay from async call?
+    getCommands: async function() {
+      try {
+        const output = await axios.get()
+        this.commandsList = output.data
+        // console.log(output.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }    
+  },
+  getters: {
+    getAllCommands: function () {
+      return this.commandsList
     }
-});
+  }
+})
